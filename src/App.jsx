e45c4192cfe63e7080bc818507d9708142247e07
Fred Silva <fredsilva015@gmail.com>
2025-04-01 18:33:43 +0100

    1.added simple main 2.added search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+import Search from './components/search';
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="bg-black flex items-center flex-col">
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1 className="text-3xl text-white">
+            Find <span>Movies</span> You'll Enjoy With out the Hassle
+          </h1>
+        </header>
+      </div>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <h1 className="text-white">{searchTerm}</h1>
+    </main>
   );
 };
 
