930f95673238e4dcf255c53bfc5042dfb7ba9e43
Fred Silva <fredsilva015@gmail.com>
2025-04-02 20:50:12 +0100

    tidy up search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="search">
-      <div>
-        🔍
-        <input
-          className="text-white"
-          type="text"
-          placeholder="Search your cozy movie!"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
+    <div className="search w-full flex justify-center">
+      <input
+        className="w-5/6 border border-white focus:outline-none text-white placeholder:text-[#FFD700] p-3 rounded-xl"
+        type="text"
+        placeholder="🔍 Search your cozy movie!"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
     </div>
   );
 };
